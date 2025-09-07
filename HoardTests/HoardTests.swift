5f1fc8d674a55897679c0e7d74583af6b1443b71
hectowatt <w477.dev@gmail.com>
2025-09-07 22:24:51 +0900

    xcodeの初期プロジェクトをコミット

---
+++ HoardTests/HoardTests.swift
@@ -0,0 +1,16 @@
+//
+//  HoardTests.swift
+//  HoardTests
+//
+//
+
+import Testing
+@testable import Hoard
+
+struct HoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
